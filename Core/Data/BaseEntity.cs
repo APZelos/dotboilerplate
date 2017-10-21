f54dd7dc42b269b022fc373e742973aa7179500b
APZelos <a.p.zelos@gmail.com>
2017-10-21 16:10:07 +0300

    Create BaseEntity abstract class

---
+++ Core/Data/BaseEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Data {
+    /// <summary>
+    /// An abstract implemention of int IBaseEnity interface.
+    /// </summary>
+    public abstract class BaseEntity : IBaseEnity<int> {
+
+        /// <summary>
+        /// The PRIMARY KEY constraint uniquely identifies each record in a database table.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// IComparable implementation.
+        /// </summary>
+        /// <param name="other">The BaseEntity that this one will be compared with.</param>
+        /// <returns></returns>
+        public int CompareTo(IBaseEnity<int> other) => this.Id - other.Id;
+    }
+}
