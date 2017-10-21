77a7c115bb6bbb2cb922f500851864d730a5f15a
APZelos <a.p.zelos@gmail.com>
2017-10-21 11:17:43 +0300

    Create IsNullOrEmpty string extension

--- Core/Utilities/Extensions.cs
+++ Core/Utilities/Extensions.cs
@@ -15,6 +15,13 @@ namespace Core.Utilities {
         /// <returns>true if the string is null or empty, or if consists exclusively of white-space characters.</returns>
         public static bool IsNullOrWhiteSpace(this string str) => string.IsNullOrWhiteSpace(str);
 
+        /// <summary>
+        /// Indicates whether the string is null or empty.
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns>true if the string is null or empty.</returns>
+        public static bool IsNullOrEmpty(this string str) => string.IsNullOrEmpty(str);
+
         #endregion Strings
     }
 }
