d375cbf9033be2baefaef90750e941a46f99acf1
APZelos <a.p.zelos@gmail.com>
2017-10-21 15:57:33 +0300

    Create IBaseEntity interface

---
+++ Core/Data/IBaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Data {
+    /// <summary>
+    /// All classes that represent a DB table
+    /// must implement this interface.
+    /// </summary>
+    public interface IBaseEntity : IComparable {
+
+        /// <summary>
+        /// The PRIMARY KEY constraint uniquely identifies each record in a database table.
+        /// </summary>
+        object Id { get; set; }
+    }
+}
