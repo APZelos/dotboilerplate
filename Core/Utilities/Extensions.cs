cefb4cdf4040db9a8a598f5db0bd434199dae738
APZelos <a.p.zelos@gmail.com>
2017-10-21 11:59:15 +0300

    Create ToTitleCase extension

--- Core/Utilities/Extensions.cs
+++ Core/Utilities/Extensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,6 +30,28 @@ namespace Core.Utilities {
         /// <param name="args">An object array that contains zero or more objects to format.</param>
         /// <returns></returns>
         public static string Format(this string str, params object[] args) => string.Format(str, args);
+
+        /// <summary>
+        /// Capitalizes each word in the string.
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="currentCulture">If true uses the current culture otherwise the invariant culture. Default: false.</param>
+        /// <returns>a string with each word capitalized.</returns>
+        public static string ToTitleCase(this string str, bool currentCulture = false) {
+            // If the string is empty or too short
+            // returns the string intact.
+            if (str.IsNullOrWhiteSpace() || str.Length < 2)
+                return str;
+            return currentCulture ?
+                CultureInfo
+                    .CurrentCulture
+                    .TextInfo
+                    .ToTitleCase(str) :
+                CultureInfo
+                    .InvariantCulture
+                    .TextInfo
+                    .ToTitleCase(str);
+        }
 
         #endregion Strings
     }
