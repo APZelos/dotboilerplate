18471cb9cecbe9fb79aa8acecff042c01c8212f9
APZelos <a.p.zelos@gmail.com>
2017-10-21 16:16:45 +0300

    Create RepositoryResult

---
+++ Core/Data/RepositoryResult.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Data {
+    /// <summary>
+    /// The result of a repository action.
+    /// </summary>
+    public class RepositoryResult<T> where T: BaseEntity {
+        /// <summary>
+        /// Indicates if the action was successfull.
+        /// </summary>
+        public bool Success { get; set; }
+
+        /// <summary>
+        /// A message returned from repository in case something went wrong.
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// The entity after the repository action (null if action was not successfull.)
+        /// </summary>
+        public T Entity { get; set; }
+
+        /// <summary>
+        /// Generic objet, placeholder for data that must be passed from repository to service.
+        /// </summary>
+        public object Obj { get; set; }
+    }
+}
