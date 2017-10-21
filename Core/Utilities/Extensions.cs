c44fbdcff030f44bfbce24f5b78abfc6fcb8bc2e
APZelos <a.p.zelos@gmail.com>
2017-10-21 11:16:00 +0300

    Create IsNullOrWhiteSpace string extension

--- Core/Utilities/Extensions.cs
+++ Core/Utilities/Extensions.cs
@@ -6,5 +6,15 @@ using System.Threading.Tasks;
 
 namespace Core.Utilities {
     public static class Extensions {
+        #region Strings
+
+        /// <summary>
+        /// Indicates whether the string is null, empty, or consists only of white-space characters.
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns>true if the string is null or empty, or if consists exclusively of white-space characters.</returns>
+        public static bool IsNullOrWhiteSpace(this string str) => string.IsNullOrWhiteSpace(str);
+
+        #endregion Strings
     }
 }
