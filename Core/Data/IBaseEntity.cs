0201212b53dafda4a79a821d2744e8dc2d464b11
APZelos <a.p.zelos@gmail.com>
2017-10-22 21:49:36 +0300

    Change IBaseEntity to an empty interface
    Change IBaseEnity to an empty interface so it can work as a top level abstraction from DB entities.

--- Core/Data/IBaseEntity.cs
+++ Core/Data/IBaseEntity.cs
@@ -7,13 +7,10 @@ using System.Threading.Tasks;
 namespace Core.Data {
     /// <summary>
     /// All classes that represent a DB table
-    /// must implement this interface.
+    /// must implement this interface. 
+    /// It's the top level abstraction of a DB entity
     /// </summary>
-    public interface IBaseEnity<T> : IComparable<IBaseEnity<T>> {
+    public interface IBaseEnity {
 
-        /// <summary>
-        /// The PRIMARY KEY constraint uniquely identifies each record in a database table.
-        /// </summary>
-        T Id { get; set; }
     }
 }
