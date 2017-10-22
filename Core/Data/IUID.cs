57703e16fa8f2e722e8892888e945f5bff5d9ee9
APZelos <a.p.zelos@gmail.com>
2017-10-22 21:53:08 +0300

    Create IUID interface

---
+++ Core/Data/IUID.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Data {
+    /// <summary>
+    /// All classes that represent a DB table
+    /// and have a unique identifier must implement this interface.
+    /// </summary>
+    /// <typeparam name="T">The type of unique identifier.</typeparam>
+    interface IUID<T> : IComparable<IUID<T>> {
+
+        /// <summary>
+        /// The PRIMARY KEY constraint uniquely identifies each record in a database table.
+        /// </summary>
+        T Id { get; set; }
+    }
+}
