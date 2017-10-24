e1ddd22ce3f07f3cfab7e388afd800107d368bb0
APZelos <a.p.zelos@gmail.com>
2017-10-24 10:32:19 +0300

    Create ITableName interface

---
+++ Core/Data/ITableName.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Data {
+    /// <summary>
+    /// Every entity that will be fetched by a
+    /// CachedByTableRepository must implement this interface.
+    /// </summary>
+    public interface ITableName {
+
+        /// <summary>
+        /// The name of the table that the entity
+        /// that implements the interface represents.
+        /// </summary>
+        string TableName { get; }
+    }
+}
