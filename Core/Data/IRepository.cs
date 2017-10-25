e8b4b9127c3a3f0c7713a77f10f61a900b06e8ce
APZelos <A.P.Zelos@intellisoft.gr>
2017-10-25 08:34:42 +0300

    Change IRepository to use Tasks

--- Core/Data/IRepository.cs
+++ Core/Data/IRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using System.Threading.Tasks;
 
 namespace Core.Data {
     /// <summary>
@@ -12,63 +13,63 @@ namespace Core.Data {
         /// </summary>
         /// <param name="id">The id of the entity that will be returned.</param>
         /// <returns>The entity with the given id.</returns>
-        T Find(object id);
+        Task<T> Find(object id);
 
         /// <summary>
         /// Inserts a new entity.
         /// </summary>
         /// <param name="entity">The entity that will be inserted.</param>
         /// <returns>a RepositoryResult.</returns>
-        RepositoryResult<T> Insert(T entity);
+        Task<RepositoryResult<T>> Insert(T entity);
 
         /// <summary>
         /// Updates an entity.
         /// </summary>
         /// <param name="entity">The entity that will be updated.</param>
         /// <returns>a RepositoryResult.</returns>
-        RepositoryResult<T> Update(T entity);
+        Task<RepositoryResult<T>> Update(T entity);
 
         /// <summary>
         /// Removes an entity.
         /// </summary>
         /// <param name="entity">The entity that will be removed.</param>
         /// <returns>a RepositoryResult.</returns>
-        RepositoryResult<T> Remove(T entity);
+        Task<RepositoryResult<T>> Remove(T entity);
 
         /// <summary>
         /// Removes an entity by id.
         /// </summary>
         /// <param name="id">The id of the entity that will be removed.</param>
         /// <returns>a RepositoryResult.</returns>
-        RepositoryResult<T> Remove(object id);
+        Task<RepositoryResult<T>> Remove(object id);
 
         /// <summary>
         /// Exposes an object that allows querying 
         /// the table of repository's entity type.
         /// </summary>
-        IQueryable<T> Query { get; }
+        Task<IQueryable<T>> Query { get; }
 
         /// <summary>
         /// Saves changes for this table.
         /// </summary>
-        void Save();
+        Task Save();
 
         /// <summary>
         /// Loads relationship objects for this entity.
         /// </summary>
         /// <param name="entity">The entity we want to load relationships for.</param>
-        void Load(T entity);
+        Task Load(T entity);
 
         /// <summary>
         /// Attaches entity to the context if is not attached.
         /// </summary>
         /// <param name="entity">The entity we want to attach to the context.</param>
-        void Attach(T entity);
+        Task Attach(T entity);
 
         /// <summary>
         /// Detaches entity from the context.
         /// </summary>
         /// <param name="entity">The entity we want to detach from the context.</param>
-        void Dettach(T entity);
+        Task Dettach(T entity);
     }
 }
