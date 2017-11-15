5246d110ff79e64df6cc413862b37d7ddfc4e364
APZelos <A.P.Zelos@intellisoft.gr>
2017-11-15 09:52:12 +0200

    Create IDependencyRegistrar

---
+++ Core/Infrastructure/IoC/IDependencyRegistrar.cs
@@ -0,0 +1,28 @@
+﻿using Autofac;
+using Core.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Infrastructure.IoC {
+    /// <summary>
+    /// The interface for dependency registration
+    /// across dlls.
+    /// </summary>
+    public interface IDependencyRegistrar {
+        /// <summary>
+        /// Register services and interfaces.
+        /// </summary>
+        /// <param name="builder">The AutoFac container builder.</param>
+        /// <param name="typeFinder">A type finder helper.</param>
+        /// <param name="config">The configuration settings.</param>
+        void Register(ContainerBuilder builder, ITypeFinder typeFinder, EngineConfig config);
+
+        /// <summary>
+        /// Indicates in what order the dependecies will be added.
+        /// </summary>
+        int Order { get; }
+    }
+}
